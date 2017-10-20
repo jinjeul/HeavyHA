800139ae1457df16e98e3e4b60a9c36748667599
jinjeul <mikael.boulo@gmail.com>
2017-10-20 12:38:39 +0000

    Update script

--- gitupdate.sh
+++ gitupdate.sh
@@ -5,7 +5,7 @@ hass --script check_config
 
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
+echo -n "Description du commit : " 
 read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
