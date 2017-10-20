34e5c9fef4d37c187d410dacd241d1ae49875803
jinjeul <mikael.boulo@gmail.com>
2017-10-20 12:25:22 +0000

    Added Update Script

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,1 +1,13 @@
-gitupdate.sh
+#!/bin/bash
+
+cd /home/pi/.homeassistant
+hass --script check_config
+
+git add .
+git status
+echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
+git commit -m "${CHANGE_MSG}"
+git push origin master
+
+exit
